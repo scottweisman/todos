f43c2659f471512a8bd3fb94439d194f9be3560c
Scott Weisman <sweisman18@gmail.com>
2013-05-20 17:15:01 -0400

    refactored todo completion spec

--- spec/features/user_toggles_todo_completion_spec.rb
+++ spec/features/user_toggles_todo_completion_spec.rb
@@ -4,20 +4,24 @@ feature	'User toggles todo completion' do
 	scenario 'marks as complete' do
 		todo = create :todo, title: 'To be completed', owner_email: 'person@example.com'
 		sign_in_as 'person@example.com'
-		expect(page).not_to have_css "[data-id='#{todo.id}'] a", text: "Incomplete"
-		find("[data-id='#{todo.id}']").click_on 'Complete'
 
-		expect(page).not_to have_css "[data-id='#{todo.id}'] a", text: "Complete"
+		expect(todo_on_page(todo)).not_to have_css 'a', text: 'Incomplete'
+		todo_on_page(todo).click_on 'Complete'
 
-		expect(page).to have_css "[data-id='#{todo.id}'][data-state='complete']"
+		expect(todo_on_page(todo)['data-state']).to eq 'complete'
+		expect(todo_on_page(todo)).not_to have_css 'a', text: 'Complete'
 	end
 
 	scenario 'marks as incomplete' do
 		todo = create :todo, title: 'To be completed', owner_email: 'person@example.com'
 		sign_in_as 'person@example.com'
-		find("[data-id='#{todo.id}']").click_on 'Complete'
-		find("[data-id='#{todo.id}']").click_on 'Incomplete'
+		todo_on_page(todo).click_on 'Complete'
+		todo_on_page(todo).click_on 'Incomplete'
 
-		expect(page).to have_css "[data-id='#{todo.id}'][data-state='incomplete']"
+		expect(todo_on_page(todo)['data-state']).to eq 'incomplete'
+	end
+
+	def todo_on_page(todo)
+		find("[data-id='#{todo.id}']")
 	end
 end
