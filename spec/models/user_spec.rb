4dfa576ef3e6445e37c308bb23b816130501ddeb
Scott Weisman <sweisman18@gmail.com>
2013-05-20 15:09:34 -0400

    added factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,8 @@ describe User, '#todoos' do
 	it 'returns todos with the correct user owner_email' do
 		user = User.new('person@example.com')
 
-		owned_todo = Todo.create! do |todo|
-			todo.owner_email = 'person@example.com'
-		end
-
-		unowned_todo = Todo.create! do |todo|
-			todo.owner_email = 'other@example.com'
-		end
+		owned_todo = create :todo, title: 'Person todo', owner_email: 'person@example.com'
+		unowned_todo = create :todo, title: 'Other todo', owner_email: 'other@example.com'
 
 		expect(user.todos).to eq [owned_todo]
 	end
