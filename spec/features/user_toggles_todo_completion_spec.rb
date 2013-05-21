f676cbc25dd86851fa150a0c63c3ee004c4eb26c
Scott Weisman <sweisman18@gmail.com>
2013-05-20 21:40:39 -0400

    got todo on page toggle tests passing

--- spec/features/user_toggles_todo_completion_spec.rb
+++ spec/features/user_toggles_todo_completion_spec.rb
@@ -2,26 +2,24 @@ require 'spec_helper'
 
 feature	'User toggles todo completion' do
 	scenario 'marks as complete' do
-		todo = create :todo, title: 'To be completed', owner_email: 'person@example.com'
 		sign_in_as 'person@example.com'
 
-		expect(todo_on_page(todo)).not_to have_css 'a', text: 'Incomplete'
-		todo_on_page(todo).click_on 'Complete'
-
-		expect(todo_on_page(todo)['data-state']).to eq 'complete'
-		expect(todo_on_page(todo)).not_to have_css 'a', text: 'Complete'
+		todo_on_page = TodoOnPage.new('To be completed')
+		todo_on_page.create
+		expect(todo_on_page).not_to have_incomplete_link
+		todo_on_page.mark_complete
+		expect(todo_on_page).to be_complete
+		expect(todo_on_page).not_to have_complete_link
 	end
 
 	scenario 'marks as incomplete' do
-		todo = create :todo, title: 'To be completed', owner_email: 'person@example.com'
 		sign_in_as 'person@example.com'
-		todo_on_page(todo).click_on 'Complete'
-		todo_on_page(todo).click_on 'Incomplete'
 
-		expect(todo_on_page(todo)['data-state']).to eq 'incomplete'
+		todo_on_page = TodoOnPage.new('To be completed')
+		todo_on_page.create
+		todo_on_page.mark_complete
+		todo_on_page.mark_incomplete
+		expect(todo_on_page).to be_incomplete
 	end
 
-	def todo_on_page(todo)
-		find("[data-id='#{todo.id}']")
-	end
 end
